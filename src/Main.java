4d29028907e785180d52a12de2764d520a6e0ea1
Em-25x <emmathehunter2007@gmail.com>
2025-09-15 23:15:16 -0400

    All done!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Given the price of a purchase the program computes and outputs a 5% sales tax.
+        // Be sure to use output statements that clearly display the price and the computed sales tax.
+        // (No conditions here, the sales tax is always the same rate.)
+
+        double priceOfPurchase = 199.87;
+        final double SALES_TAX = .05;
+        double finalPrice = 0;
+        finalPrice = priceOfPurchase * (SALES_TAX + 1);
+
+        System.out.println("The final price of a purchase of " + priceOfPurchase + " at " + SALES_TAX + "% sales tax is $" +finalPrice);
     }
 }
